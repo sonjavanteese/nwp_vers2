b802edc83f5acc6209d7ee9ce9ed7793632af1c4
sonjavanteese <35209042+sonjavanteese@users.noreply.github.com>
2021-11-24 15:44:25 +0100

    add icons

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,97 +1,107 @@
- <script>
-    import '../app.css'
-    import {iconlib} from '$lib/header/icons'
-    import {fade} from 'svelte/transition';
-    import Header from '$lib/header/Header.svelte';
-    import { page } from '$app/stores';
- </script>
-<div class="w-0 h-0">
+<script>
+	import { Icon, iconlib } from '$lib/nwp-icons/'
+  import "../app.css"
+  import { page } from '$app/stores';
+	// import logo from './svelte-logo.svg';
+	// import logoDark from './svelte-logo-dark.svg';
+  $: console.log($page)
+</script>
 {@html iconlib}
-</div> 
-<Header />
-{#key $page.path}
-  <main out:fade={{duration: 200}} in:fade={{duration: 400, delay: 220}}>
-  <slot />
-  </main>  
-{/key}
 
-<footer class="px-4 py-2 text-center">{$page.path === '/' ? `/start` : $page.path}</footer>
+
+<header class="bg-blue-500 text-white shadow">
+  <nav class="flex justify-between items-center container mx-auto py-2 px-4">
+    <a href="/" class="text-2xl"><Icon name="home" /></a>
+    <span class="text-3xl font-semibold">nwp-app</span>
+    <a href="/icons" class="text-2xl"><Icon name="cog" /></a>
+  </nav>
+</header>
+
+
+<main>
+ <slot /> 
+</main>
+
+
 <style windi:preflights:global windi:safelist:global global>
-    :root {
-      --font-mono: 'Fira Mono', monospace;
-      --pure-white: #ffffff;
-      --primary-color: #b9c6d2;
-      --secondary-color: #d0dde9;
-      --tertiary-color: #edf0f8;
-      --accent-color: #ff3e00;
-      --heading-color: rgba(0, 0, 0, 0.7);
-      --text-color: #444444;
-      --background-without-opacity: rgba(255, 255, 255, 0.7);
-      --column-width: 42rem;
-      --column-margin-top: 4rem;
-      --app-body-color: #424656;
-      --app-body-bg: #FFFFFF;
-      --app-bar-color: #E0E0E0;
-      --app-bar-bg: #2172D2;
-      --app-bar-border: 1px solid #dddddd;
-      --app-bar-item-h: 56px;
-      --app-bar-item-w: 48px;
-      --column-width: 680px;
-      --column-margin-top: 1rem;
-      --column-padding: 1rem;
-    }
+  :root {
+    --font-mono: 'Fira Mono', monospace;
+    --pure-white: #ffffff;
+    --primary-color: #b9c6d2;
+    --secondary-color: #d0dde9;
+    --tertiary-color: #edf0f8;
+    --accent-color: #ff3e00;
+    --heading-color: rgba(0, 0, 0, 0.7);
+    --text-color: #444444;
+    --background-without-opacity: rgba(255, 255, 255, 0.7);
+    --column-width: 42rem;
+    --column-margin-top: 4rem;
+    --app-body-color: #424656;
+    --app-body-bg: #FFFFFF;
+    --app-bar-color: #E0E0E0;
+    --app-bar-bg: #2172D2;
+    --app-bar-border: 1px solid #dddddd;
+    --app-bar-item-h: 56px;
+    --app-bar-item-w: 48px;
+    --column-width: 680px;
+    --column-margin-top: 1rem;
+    --column-padding: 1rem;
+  }
 
 h1,
 h2,
 h3,
 h4,
 h5 {
-  @apply text-5xl mt-2 mb-6 leading-tight font-heading;
+@apply text-5xl mt-2 mb-6 leading-tight font-heading;
 }
 
 h1 {
-  @apply text-6xl;
+@apply text-6xl;
 }
 
 h2 {
-  @apply text-5xl;
+@apply text-5xl;
 }
 
 h3 {
-  @apply text-4xl;
+@apply text-4xl;
 }
 
 h4 {
-  @apply text-3xl;
+@apply text-3xl;
 }
 
 p {
-  @apply text-xl mb-2 text-gray-500 leading-relaxed;
+@apply text-xl mb-2 text-gray-500 leading-relaxed;
 }
 
 .lead {
-  @apply text-2xl;
+@apply text-2xl;
 }
 .select {
-  @apply appearance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-200 focus:bg-white border border-gray-200 focus:border-gray-500 rounded focus:outline-none;
+@apply appearance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-200 focus:bg-white border border-gray-200 focus:border-gray-500 rounded focus:outline-none;
 }
 .input {
-  @apply appearance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-50 focus:bg-white border border-gray-200 focus:border-gray-500 rounded focus:outline-none;
+@apply appearance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-50 focus:bg-white border border-gray-200 focus:border-gray-500 rounded focus:outline-none;
 }
 .label {
-  @apply block mb-2 text-xs text-gray-700 uppercase tracking-wide font-bold;
+@apply block mb-2 text-xs text-gray-700 uppercase tracking-wide font-bold;
 }
 .input-field {
-  @apply appearance-none block w-full py-3 px-4 text-gray-400 bg-white border border-gray-200 rounded focus:border-gray-200 focus:outline-none;
+@apply appearance-none block w-full py-3 px-4 text-gray-400 bg-white border border-gray-200 rounded focus:border-gray-200 focus:outline-none;
 }
 .button {
-  @apply inline-block w-full py-4 px-8 leading-none text-white bg-blue-600 hover:bg-blue-700 font-semibold rounded shadow;
+@apply inline-block w-full py-4 px-8 leading-none text-white bg-blue-600 hover:bg-blue-700 font-semibold rounded shadow;
 }
 .button[disabled] {
-  @apply  text-gray-400 bg-gray-200 hover:bg-gray-200 shadow-none cursor-not-allowed;
+@apply  text-gray-400 bg-gray-200 hover:bg-gray-200 shadow-none cursor-not-allowed;
 }
 
 .page-container {
-    @apply w-full max-w-lg mx-auto px-4 py-4;
+  @apply min-h-full w-full max-w-xl mx-auto px-4 py-4 shadow;
+}
+.head-container {
+  @apply w-full max-w-xl mx-auto px-4 py-4 shadow;
 }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,25 @@
 <script>
-    export let name = "Start"
+  import { Icon, iconlist } from "$lib/nwp-icons/";
 </script>
 
+
 <section class="page-container">
-<h1>Welcome to SvelteKit</h1>
-<h2>{name}</h2>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  <nav class="flex items-center mb-4"><span class="flex-grow"></span><a href="/icons" class="text-blue-800 text-lg font-medium">Icon Libary</a></nav>
+  <h1 class="py-4 text-center border-b mb-8">Nwp Icon List</h1>
+
+  <div class="flex flex-col divide-y divide-opacity-10">
+    {#each iconlist as icon}
+      <a href="/icons" class="flex items-center px-4 py-3">
+          <div class="text-2xl font-medium">{icon.toUpperCase()}</div>
+          <span class="flex-grow"></span>
+          <Icon name={icon} />
+      </a>
+    {/each}
+  </div> 
 </section>
+
+
+<style>
+  
+
+</style>
